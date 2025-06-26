c997ea70fd56d6e82b5680e5523af7a3374736c6
niklasschlueter <niklas.schlueter@tu-berlin.de>
2025-06-26 10:41:54 +0200

    restructuring and creation of datapoints

--- src/plots.jl
+++ src/plots.jl
@@ -1,22 +1,50 @@
 using GLMakie
 
-function loss_plot!(fig, vis_loss::Observable{Vector{Float32}})
-    #ax_loss = Axis(fig[1, 1], xlabel = "x label", ylabel = "y label", title = "Losses")
-    #lines!(ax_loss, @lift(1:length($vis_loss)), vis_loss)
-    
+include("util.jl")
+
+struct plot_data_losses
+    losses::Observable{Vector{Float32}}
+end
+
+function loss_plot!(fig, datapoints::Observable{Vector{datapoint}})
+    pd_losses = plot_data_losses(Observable{Vector{Float32}}([]))
+
     ax_loss = Axis(fig[1, 1], xlabel = "Iteration", ylabel = "Loss", title = "Training Loss")
-    loss_line = lines!(ax_loss, vis_loss, label = "Training Loss", color = :blue)
+    loss_line = lines!(ax_loss, pd_losses.losses, label = "Training Loss", color = :blue)
+
     axislegend(ax_loss)
 
-    on(vis_loss) do vl
-        if length(vl) > 1
-            xlims!(ax_loss, 1, length(vl))
-            ylims!(ax_loss, minimum(vl), maximum(vl))
+    on(datapoints) do data
+        @info(typeof(data[end]))
+        push!(pd_losses.losses, data[end].loss)
+    end
+
+    on(pd_losses.losses) do losses
+        if length(losses) > 1
+            xlims!(ax_loss, 1, length(losses))
+            ylims!(ax_loss, minimum(losses), maximum(losses))
         end
     end
 
     return ax_loss
 end
+
+#=struct grad_norm_plot
+    grad_norm::Observable{Vector{Float32}}
+end
+function grad_norm_plot(fig, vis_grad::Observable{Vector{Float32}})
+    ax_grad = Axis(fig[1, 2], xlabel = "x label", ylabel = "y label", title = "Gradient Norms")
+
+    on(vis_grad) do gn
+        if length(gn) > 1
+            xlims!(ax_grad, 1, length(gn))
+            ylims!(ax_grad, minimum(gn), maximum(gn))
+        end
+    end
+
+
+end
+
 
 function grad_norm_plot!(fig, vis_grad_norm::Observable{Vector{Float32}})
     ax_grad = Axis(fig[1, 2], xlabel = "x label", ylabel = "y label", title = "Gradient Norms")
@@ -30,4 +58,4 @@ function grad_norm_plot!(fig, vis_grad_norm::Observable{Vector{Float32}})
     end
 
     return ax_grad
-end
+end=#
