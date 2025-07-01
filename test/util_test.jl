e0e372b9a4f9f7dcec566a6a002b30411fd5d98b
tldnjs0774 <tldnjs0774@gmail.com>
2025-07-01 15:50:53 +0200

    Delete comments

--- test/util_test.jl
+++ test/util_test.jl
@@ -16,28 +16,3 @@ include("../src/util.jl")
     push!(obs_int, 7)
     @test obs_int[] == [1, 2, 7]
 end
-
-#function push!(obs::Observable{Vector{Datapoint}}, dp::Datapoint)
-# struct Datapoint
-#     epoch::Int
-#     batch::Int
-#     loss::Union{Float32, Nothing}
-#     grads::Union{@NamedTuple{Any}, Nothing}
-#     params::Zygote.Params{Zygote.Buffer{Any, Vector{Any}}}
-# end
-# @testset "push! with Datapoint" begin
-#     buffer = Zygote.Buffer([1.0, 2.0])
-#     params = Zygote.Params([buffer])
-#     grads = (; g1=1.0, g2=2.0)
-    
-#     dp1 = Datapoint(1, 1, 0.5f0, grads, params)
-#     dp2 = Datapoint(1, 2, 0.3f0, nothing, params)
-    
-#     obs = Observable([dp1])
-#     push!(obs, dp2)
-#     @test obs[] == [dp1, dp2]
-# end
-
-@testset "append!" begin
-    
-end
