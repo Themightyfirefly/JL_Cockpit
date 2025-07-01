3a7ad157099b3705099bf251f330d9b89d0004f3
Themightyfirefly <niklschlueter@gmail.com>
2025-07-01 16:13:32 +0200

    including packages and exports

--- src/jl_cockpit.jl
+++ src/jl_cockpit.jl
@@ -1,22 +1,16 @@
-using GLMakie
-using LinearAlgebra
-using Zygote
-using Flux
-using MLDatasets
-using Statistics
-
 module jl_cockpit
-
-    # plots.jl is used to define how each individual plot looks like
-    #include("plots.jl")
-
-    # Here the training loop is defined and executed. 
+    using GLMakie
+    using LinearAlgebra
+    using Zygote
+    using Flux
+    using MLDatasets
+    using Statistics
+ 
     include("training_loop.jl")
 
-    # Used to store the observables and manages the visualisation
-    #include("visualiser.jl")
 
-    # Extra functions (mostly additions to general functions via multiple dispatch)
-    #include("util.jl")
+    export visualiser
+
+    export training_loop
 
 end
